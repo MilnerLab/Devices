94b41b9e878761ed2e61fe7a48adbafd20168395
soerenmahr <soerenmahr.srm@gmail.com>
2025-12-31 16:57:02 -0800

    ell

--- elliptec/elliptec_ell14.py
+++ elliptec/elliptec_ell14.py
@@ -5,29 +5,32 @@ from typing import Optional
 from base_core.math.enums import AngleUnit
 from base_core.math.models import Angle, Range
 from elliptec.base.elliptec_device import ElliptecDevice
+from phase_control.io.rotator.ui.rotator_settings_vm import ELL14Config
 
-ANGLE_RANGE = Range(Angle(-90, AngleUnit.DEG), Angle(90, AngleUnit.DEG))
-OUT_OF_RANGE_RELATIVE_ANGLE = Angle(90, AngleUnit.DEG)
-HOME_ANGLE = Angle(0, AngleUnit.DEG)
 COUNTS_PER_REV = 262_144  # ELL14: 262144 pulses/rev (0x40000) :contentReference[oaicite:3]{index=3}
 
 class Rotator(ElliptecDevice):
     def __init__(
-        self
+        self,
+        config: ELL14Config
     ) -> None:
         super().__init__()
-        self._current_angle: float = 0
+        self._config = config
+        self._current_angle: Angle = Angle(0, AngleUnit.DEG, wrap= False)
 
+    def apply_config(self):
+        self.set_speed(self._config.speed)
 
     def rotate(self, angle: Angle) -> None:
         
         if float(angle) == 0.0:
             return
 
-        new_angle = self._current_angle + angle.Deg
-        # self._validate_new_delta_angle(new_angle)
+        new_angle = Angle(self._current_angle + angle, wrap= False)
+        self._validate_new_delta_angle(new_angle)
         self._move_relative(angle)
-        
+        print("Current wp angle:", self._current_angle.Deg)
+        print("--------------------------")
 
 
     # ------------------------------------------------------------------    
@@ -36,26 +39,23 @@ class Rotator(ElliptecDevice):
 
 
     def _move_relative(self, angle: Angle) -> None:
-        self._current_angle = self._current_angle + angle.Deg
-        print("Current wp angle:", self._current_angle)
-        print("--------------------------")
         self.move_relative(self._angle_to_counts(angle))
+        self._current_angle = Angle(self._current_angle + angle, wrap= False)
+        time.sleep(2.0)
 
     def _validate_new_delta_angle(self, new_angle: Angle) -> None:
         
-        if ANGLE_RANGE.is_in_range(new_angle):
+        if self._config.angle_range.is_in_range(new_angle):
             return
 
-        if new_angle > ANGLE_RANGE.max:
-            correction = Angle(-OUT_OF_RANGE_RELATIVE_ANGLE)
+        if new_angle > self._config.angle_range.max:
+            correction = Angle(-self._config.out_of_range_rel_angle)
             print("corrected max")
         else:
-            correction = OUT_OF_RANGE_RELATIVE_ANGLE
+            correction = self._config.out_of_range_rel_angle
             print("corrected min")
 
         self._move_relative(correction)
         
     def _angle_to_counts(self, angle: Angle) -> int:
         return int(round(angle.Rad / (2.0 * math.pi) * COUNTS_PER_REV))
-        
-    
